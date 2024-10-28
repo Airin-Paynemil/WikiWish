25eb4076420f8e0208a26e22ebe9fdd4afb0dc0c
Airin-Paynemil <airin.paynemil.epet20@gmail.com>
2024-10-28 00:06:55 -0300

    se agrego calculadora

---
+++ js/calculadora-enlases.js
@@ -0,0 +1,11 @@
+document.getElementById("boton1").onclick = function() {
+    window.location.href = "../calculadoraPersonajes.html";
+};
+
+document.getElementById("boton2").onclick = function() {
+    window.location.href = "../calculadoraArmas.html";
+};
+
+document.getElementById("boton3").onclick = function() {
+    window.location.href = "../calculadoraPersonajes.html";
+};
---
+++ js/index-enlases.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("miBoton1").onclick = function() {
+        window.location.href = "../directorio_personajes.html";
+    };
+
+    document.getElementById("miBoton2").onclick = function() {
+        window.location.href = "../directorio_armas.html";
+    };
+
+    document.getElementById("miBoton3").onclick = function() {
+        window.location.href = "../directorio_enemigos.html";
+    };
+
+    document.getElementById("miBoton4").onclick = function() {
+        window.location.href = "../calculadora.html";
+    };
+
+    document.getElementById("miBoton5").onclick = function() {
+        window.location.href = "../historia_deseos.html";
+    };
+
+    document.getElementById("miBoton6").onclick = function() {
+        window.location.href = "../historia_deseos.html";
+    };
+});
